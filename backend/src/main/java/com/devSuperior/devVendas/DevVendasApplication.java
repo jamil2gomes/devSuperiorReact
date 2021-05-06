10071f3eae8c259c0dc9ba96d0f774531fe13fd2
jamil <jamil.gomes@armateus.com.br>
2021-05-06 09:01:43 -0300

    Project created

---
+++ backend/src/main/java/com/devSuperior/devVendas/DevVendasApplication.java
@@ -0,0 +1,13 @@
+package com.devSuperior.devVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevVendasApplication.class, args);
+	}
+
+}
